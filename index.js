9699bd141debcb47a83f19df0c55871e06550d0b
George2013 <jorge.hernandez@qrvey.com>
2021-08-30 15:11:46 -0500

    .

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const app = require('express')();
+const cors = require('cors');
+const express = require('express');
+const app = express();
+var corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+app.use(cors(corsOptions));
 const http = require('http').createServer(app);
 const socketIo= require('socket.io');
 const io = socketIo(http);
 const PORT = process.env.PORT || 5000;
 
 
+
 io.on('connection',(socket) =>{
-  console.log('an user was connected')
+  console.log('an user was connected');
+  console.log('socet id', socket.id)
+  socket.on('create-room',name=>{
+    console.log('name ----- ',name)
+  })
 });
 
 http.listen(PORT, () => {
