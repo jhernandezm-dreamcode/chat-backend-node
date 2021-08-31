ad2a62e547fc9d711cbf991016336abae4bf500a
George2013 <jorge.hernandez@qrvey.com>
2021-08-31 10:12:17 -0500

    remove user

--- server/helpers/helper.js
+++ server/helpers/helper.js
@@ -8,7 +8,16 @@ const addUser = ({ socket_id, name, user_id, room_id }) => {
   }
   const user = { socket_id, name, user_id, room_id };
   users.push(user);
+  console.log('users--',users)
   return { user: user };
 };
+
+const removeUser = (socket_id) =>{
+    const index = user.findIndex(user => user.socket_id === socket_id);
+    if(index !== -1){
+        return users.splice(index,1)[0];
+    }
+}
+
 
 module.exports = {addUser}
